3f8b4354b5ad4b10ff0f451642fa9047718976ac
mingfun wong <mingfun.wong.chn@gmail.com>
2018-09-03 00:27:20 +0900

    add data

--- data.js
+++ data.js
@@ -10,10 +10,13 @@ var data = {
           child: [
             {name: "FIBOS ROCKS", desc: "FIBOS 区块浏览器", url: "http://explorer.fibos.rocks/"},
             {name: "FIBOS.IO", desc: "FIBOS 官方网站", url: "https://fibos.io/"},
+            {name: "MORE Wallet", desc: "首款支持兑换 FO 的钱包", url: "https://more.top/"},
+            {name: "51Token", desc: "支持 FO 闪兑的钱包", url: "http://www.51token.im/"},
+            {name: "FO 钱包", desc: "基于 FIBOS 生态系统的智能钱包", url: "http://wallet.fo/"},
+            {name: "FibosBP", desc: "FIBOS 在线工具包", url: "http://fibosbp.com/"},
             {name: "开发文档", desc: "FIBOS 开发文档", url: "https://fibos.io/docs/guide/basic/about.md.html"},
             {name: "Telegram", desc: "FIBOS 官方电报群", url: "https://t.me/FIBOSIO"},
             {name: "社区", desc: "FIBOS 官方社区", url: "http://bbs.dev.fo/"},
-            {name: "GITHUB", desc: "FIBOS 源代码", url: "https://github.com/FIBOSIO"},
             {name: "Fibos Startup", desc: "FIBOS 常用代码", url: "https://github.com/dabdevelop/fibos-startup/"},
           ]
         }
