06153f25cce7ddc1d54b28f7443e6038a196ea66
mingfun wong <mingfun.wong.chn@gmail.com>
2018-09-03 00:37:44 +0900

    add data

--- data.js
+++ data.js
@@ -14,9 +14,9 @@ var data = {
             {name: "51Token", desc: "支持 FO 闪兑的钱包", url: "http://www.51token.im/"},
             {name: "FO 钱包", desc: "基于 FIBOS 生态系统的智能钱包", url: "http://wallet.fo/"},
             {name: "FibosBP", desc: "FIBOS 在线工具包", url: "http://fibosbp.com/"},
+            {name: "FO IRONMAN", desc: "使用 Ironman 插件可以轻松双向兑换", url: "http://foironman.com/"},
             {name: "开发文档", desc: "FIBOS 开发文档", url: "https://fibos.io/docs/guide/basic/about.md.html"},
             {name: "Telegram", desc: "FIBOS 官方电报群", url: "https://t.me/FIBOSIO"},
-            {name: "社区", desc: "FIBOS 官方社区", url: "http://bbs.dev.fo/"},
             {name: "Fibos Startup", desc: "FIBOS 常用代码", url: "https://github.com/dabdevelop/fibos-startup/"},
           ]
         }
@@ -33,18 +33,19 @@ var data = {
           ]
         },
         {
-          name: "工具",
-          child: [
-            {name: "Fibos Startup", desc: "FIBOS 常用代码", url: "https://github.com/dabdevelop/fibos-startup/"},
-            {name: "FibosBP", desc: "FIBOS 在线工具包", url: "http://fibosbp.com/"},
-          ]
-        },
-        {
           name: "钱包",
           child: [
             {name: "MORE Wallet", desc: "首款支持兑换 FO 的钱包", url: "https://more.top/"},
             {name: "51Token", desc: "支持 FO 闪兑的钱包", url: "http://www.51token.im/"},
             {name: "FO 钱包", desc: "基于 FIBOS 生态系统的智能钱包", url: "http://wallet.fo/"},
+          ]
+        },
+        {
+          name: "工具",
+          child: [
+            {name: "FibosBP", desc: "FIBOS 在线工具包", url: "http://fibosbp.com/"},
+            {name: "FO IRONMAN", desc: "使用 Ironman 插件可以轻松双向兑换", url: "http://foironman.com/"},
+            {name: "Fibos Startup", desc: "FIBOS 常用代码", url: "https://github.com/dabdevelop/fibos-startup/"},
           ]
         },
         {
