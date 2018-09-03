eaa1da8e73a5afce1a1910ec81ccdbbf643966a4
mingfun wong <mingfun.wong.chn@gmail.com>
2018-09-03 20:48:01 +0900

    add title switch

--- public/js/app.js
+++ public/js/app.js
@@ -5,16 +5,27 @@ AOS.init({
 });
 
 var eos2fo, fo2eos;
+var i = 0;
 
 getExchangeInfo();
+
+setInterval(function () {
+	if (eos2fo) {
+	    if (i % 2 == 0) {
+			document.title = eos2fo + " FO / EOS | FIBOS 导航"
+	    } else {
+			document.title = fo2eos.toFixed(6) + " EOS / FO | FIBOS 导航"
+	    }
+	    i++;
+    }
+}, 3 * 1000)
 
 function getExchangeInfo() {
   var url = "https://json2jsonp.com/?url=" + encodeURIComponent('https://fibos.io/getExchangeInfo?rand='+Math.random()) + "&callback=?";
   $.getJSON(url, function(data){
     eos2fo = data.price
     fo2eos = 1 / data.price
-    document.title = eos2fo + " FO / EOS | FIBOS 导航"
-    setTimeout(getExchangeInfo, 1000)
+    setTimeout(getExchangeInfo, 2000)
   });
 }
 
