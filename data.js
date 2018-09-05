4679afbef55e9e2ee7cc4170bf4832123d174d02
mingfun wong <mingfun.wong.chn@gmail.com>
2018-09-06 02:26:06 +0900

    update data

--- data.js
+++ data.js
@@ -24,16 +24,16 @@ var data = {
         {
           name: "",
           child: [
-            {name: "FIBOS ROCKS", desc: "FIBOS 区块浏览器", url: "http://explorer.fibos.rocks/"},
+            {name: "FIBOS ROCKS", icon: "fas fa-tachometer-alt", desc: "一款出色的 FIBOS 区块链浏览器", url: "http://explorer.fibos.rocks/"},
+            {name: "Ironman", icon: "fas fa-wrench", desc: "使用 Ironman 插件可以轻松双向兑换", url: "http://foironman.com/"},
+            {name: "FOToolkit", icon: "fas fa-toolbox", desc: "FIBOS 工具套件。安全，可靠，透明和快速", url: "https://fotoolkit.com/home"},
             {name: "FIBOS.IO", desc: "FIBOS 官方网站", url: "https://fibos.io/"},
             {name: "MORE Wallet", desc: "支持傻瓜式一键投兑 FO 功能", url: "https://more.top/"},
             {name: "51Token", desc: "支持 FO 闪兑的钱包", url: "http://www.51token.im/"},
             {name: "FO 钱包", desc: "基于 FIBOS 生态系统的智能钱包", url: "http://wallet.fo/"},
-            {name: "Ironman", desc: "使用 Ironman 插件可以轻松双向兑换", url: "http://foironman.com/"},
-            {name: "FibosBP", desc: "FIBOS 在线工具包", url: "http://fibosbp.com/"},
             {name: "开发文档", desc: "FIBOS 开发文档", url: "https://fibos.io/docs/guide/basic/about.md.html"},
-            {name: "Telegram", desc: "FIBOS 官方电报群", url: "https://t.me/FIBOSIO"},
-            {name: "Fibos Startup", desc: "FIBOS 常用代码", url: "https://github.com/dabdevelop/fibos-startup/"},
+            {name: "imondo", desc: "由 fibos4imondo 超级节点 建立的一个活跃的小众社区", url: "http://imondo.io/"},
+            {name: "GitHub", desc: "FIBOS 源代码", url: "https://github.com/FIBOSIO"},
           ]
         }
       ]
@@ -45,16 +45,16 @@ var data = {
         {
           name: "浏览器",
           child: [
-            {name: "FIBOS ROCKS", icon: "fas fa-tachometer-alt", desc: "FIBOS 区块浏览器", url: "http://explorer.fibos.rocks/"},
+            {name: "FIBOS ROCKS", icon: "fas fa-tachometer-alt", desc: "一款出色的 FIBOS 区块链浏览器", url: "http://explorer.fibos.rocks/"},
           ]
         },
         {
           name: "钱包",
           icon: "",
           child: [
-            {name: "MORE Wallet", desc: "支持傻瓜式一键投兑 FO 功能", url: "https://more.top/"},
-            {name: "51Token", desc: "支持 FO 闪兑的钱包", url: "http://www.51token.im/"},
-            {name: "FO 钱包", desc: "基于 FIBOS 生态系统的智能钱包", url: "http://wallet.fo/"},
+            {name: "MORE Wallet", desc: "支持傻瓜式一键投兑 FO 功能", url: "https://more.top/", more: {icon: "", name: "教程", url: "https://www.jianshu.com/p/4b1f2f2825f7"}},
+            {name: "51Token", desc: "支持 FO 闪兑的钱包", url: "http://www.51token.im/", more: {icon: "", name: "教程", url: "http://imondo.io/article/1535949163876"}},
+            {name: "FO 钱包", desc: "基于 FIBOS 生态系统的智能钱包", url: "http://wallet.fo/", more: {icon: "", name: "教程", url: "https://fibos.io/5b8b51baaba908441dbbfc81.html"}},
             {name: "OneChain", desc: "数字货币全能 APP", url: "http://www.onechain.one/"},
           ]
         },
@@ -74,7 +74,7 @@ var data = {
             {name: "开发文档", desc: "FIBOS 开发文档", url: "https://fibos.io/docs/guide/basic/about.md.html"},
             {name: "Telegram", desc: "FIBOS 官方电报群", url: "https://t.me/FIBOSIO"},
             {name: "社区", desc: "FIBOS 官方社区", url: "http://bbs.dev.fo/"},
-            {name: "GITHUB", desc: "FIBOS 源代码", url: "https://github.com/FIBOSIO"},
+            {name: "GitHub", desc: "FIBOS 源代码", url: "https://github.com/FIBOSIO"},
           ]
         },
         {
