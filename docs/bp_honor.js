592d72868efc317b8fd690906eaf35c224705592
mingfun wong <mingfun.wong.chn@gmail.com>
2018-11-11 01:15:49 +0900

    add data

--- docs/bp_honor.js
+++ docs/bp_honor.js
@@ -1,7 +1,6 @@
 var bp_honor = {
-	"fibosrockskr": ["Fibos Rocks 浏览器", "Dapp 开发者", "节点英雄", "51Token"],
+	"fibosrockskr": ["Fibos Rocks 浏览器", "51Token 钱包", "Dapp 开发者", "节点英雄"],
 	"fibos123comm": ["FIBOS 导航", "节点英雄"],
-	"fibscandotio": ["FIBSCAN 浏览器"],
 	"ansenironman": ["IronMan 作者"],
 	"lovefibosb": ["Fotoolkit 作者"],
 	"fibosisfunoo": ["Dapp 开发者"],
@@ -11,10 +10,13 @@ var bp_honor = {
 	"eosteaeostea": ["节点英雄"],
 
 	"liuqiangdong": ["FIBOS 数据备份", "60% 分红"],
-	"lijialintest": ["60% 分红"],
 	"zhangsenyuan": ["Dapp 开发者", "60% 分红"],
+	"fibscandotio": ["FIBSCAN 浏览器", "60% 分红"],
+	"lijialintest": ["60% 分红"],
 	"superbpusphl": ["60% 分红"],
 	"shaoshaoshao": ["60% 分红"],
 	"williamfouvy": ["60% 分红"],
 	"singaporebps": ["60% 分红"],
+	"ukfiboooooos": ["60% 分红"],
+	"fotothemooon": ["60% 分红"],
 };
