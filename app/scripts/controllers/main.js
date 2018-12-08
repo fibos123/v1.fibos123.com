1d1132c4cef54c73aa84f6c8b1ee08a601651dec
mingfun wong <mingfun.wong.chn@gmail.com>
2018-12-09 05:25:51 +0900

    sites data move to contract

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,7 +13,23 @@ angular.module('appApp')
   	document.title = 'FIBOS 导航';
   	$(window).scrollTop(0)
 
-	$scope.data = websites;
+		// url.rpc.get_table_rows = "http://127.0.0.1:8888/v1/chain/get_table_rows"
+		util.ajax({url: url.rpc.get_table_rows, data:
+			JSON.stringify({
+				"json": "true",
+				"code": "fibos123comc",
+				"scope": "fibos123comc",
+				"table": "jsons",
+				"limit": 1,
+				"lower_bound": "sites",
+			}), type: "POST"}
+		, function(data) {
+			if (data && data.rows && data.rows[0] && data.rows[0].text) {
+				var websites = data.rows[0].text;
+				$scope.data = JSON.parse(websites);
+				$scope.$apply();
+			}
+		}, function (){})
 
 	// AOS.init({
 	//   startEvent: 'aosEvent',
