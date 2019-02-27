5e8103b227151b3a8526c7a779bb2ac232dcb77c
mingfun wong <mingfun.wong.chn@gmail.com>
2019-02-27 18:11:34 +0900

    refine

--- app/scripts/controllers/bp.js
+++ app/scripts/controllers/bp.js
@@ -8,219 +8,222 @@
  * Controller of the appApp
  */
 angular.module('appApp')
-  .controller('BpCtrl', function ($scope) {
+	.controller('BpCtrl', function ($scope) {
 
-  	document.title = '节点列表 | FIBOS 导航';
-  	$(window).scrollTop(0)
+		document.title = '节点列表 | FIBOS 导航';
+		$(window).scrollTop(0)
 
 		if (window.location.protocol === "https:") {
 			window.location.href = "http:" + window.location.href.substring(window.location.protocol.length);
 			return;
 		}
-		
-  	var items = [];
+
+		var items = [];
 		var is_set = false;
 		var global = {}; // 计算收益
 		var info = {}; // 最新区块
 		var totalVotessum = 0;
-  	var st1;
-  	var bpname2i = {};
+		var st1;
+		var bpname2i = {};
 
-  	$scope.bp_honor = util.copy(bp_honor);
-  	$scope.refresh = main;
-
-  	main();
-	get_info();
-
-  	function main(){
-  		bpname2i = {};
 		$scope.bp_honor = util.copy(bp_honor);
-	  	get_global(function(data){
-	  		global = data.rows[0];
-		  	get_producers(function(data) {
-		  		items = data.rows;
-				totalVotessum = util.totalVotessum(data.rows);
-		  		for (var i = 0; i < items.length; i++) {
-		  			var bp = items[i];
-		  			items[i]["rank"] = i + 1;
-		  			items[i]["history"] = {};
-		  			items[i]["history"]["weekpercent"] = 0;
-		  			items[i]["staked"] = util.getStaked(bp.total_votes) 
-		  			items[i]["weight_percent"] = util.weightPercent(bp.total_votes, totalVotessum)
-		  			var getClaimRewards = util.getClaimRewards(bp, global, items[i]["rank"]);
-		  			items[i]["claim_rewards_total"] = getClaimRewards.total;
-					items[i]["claim_rewards_unreceived"] = getClaimRewards.unreceived;
-					items[i]["http"] = {
-						status: "un",
-						msg: "unset",
-						endpoint: "",
-						version: "",
-						number: 0,
-						history: false,
-						cors: true
-					};
-					
-					items[i]["https"] = {
-						status: "un",
-						msg: "unset",
-						endpoint: "",
-						version: "",
-						number: 0,
-						history: false,
-						cors: true
-					};
-					items[i]["p2p"] = {
-						status: "un",
-						msg: "unset",
-						endpoint: "",
-						detecting: true
-					};
+		$scope.refresh = main;
+
+		main();
+		get_info();
+
+		function main() {
+			bpname2i = {};
+			$scope.bp_honor = util.copy(bp_honor);
+			get_global(function (data) {
+				global = data.rows[0];
+				get_producers(function (data) {
+					items = data.rows;
+					totalVotessum = util.totalVotessum(data.rows);
+					for (var i = 0; i < items.length; i++) {
+						var bp = items[i];
+						items[i]["rank"] = i + 1;
+						items[i]["history"] = {};
+						items[i]["history"]["weekpercent"] = 0;
+						items[i]["staked"] = util.getStaked(bp.total_votes)
+						items[i]["weight_percent"] = util.weightPercent(bp.total_votes, totalVotessum)
+						var getClaimRewards = util.getClaimRewards(bp, global, items[i]["rank"]);
+						items[i]["claim_rewards_total"] = getClaimRewards.total;
+						items[i]["claim_rewards_unreceived"] = getClaimRewards.unreceived;
+						items[i]["http"] = {
+							status: "un",
+							msg: "unset",
+							endpoint: "",
+							version: "",
+							number: 0,
+							history: false,
+							cors: true
+						};
+
+						items[i]["https"] = {
+							status: "un",
+							msg: "unset",
+							endpoint: "",
+							version: "",
+							number: 0,
+							history: false,
+							cors: true
+						};
+						items[i]["p2p"] = {
+							status: "un",
+							msg: "unset",
+							endpoint: "",
+							detecting: true
+						};
 
 
-					bpname2i[bp["owner"]] = i;
-		  		}
-				is_set = true;
-				get_producerjson();
-		  	}, function(){})
-	  	})
-  	}
-
-  	function get_info () {
-  		util.ajax({url: url.rpc.get_info}, function(data){
-	  		info = data;
-			$scope.info = info;
-			setTimeout(function(){
-				$(".progress-bar").width("100%");
-			}, 1);
-			$scope.$apply();
-			clearTimeout(st1);
-			st1 = setTimeout(function (){
-				get_info()
-			}, 1000)
-  		}, function(){})
-  	}
-
-
-	function set() {
-  		$scope.items = items;
-		$scope.$apply();
-		// $(".tooltip").remove();
-	  	$('[data-toggle="tooltip"]').tooltip();
-	}
-
-	var si1 = setInterval(function (){
-  		if (is_set) {
-  			is_set = false;
-  			set()
-  		}
-	}, 200);
-
-	$scope.$on("$destroy", function() {
-		clearInterval(si1);
-		clearTimeout(st1);
-	})
-
-  	function get_global(success, error){
-  		util.ajax(
-  			{
-  				url: url.rpc.get_table_rows, 
-  				type: "POST", 
-  				data: JSON.stringify({
-		  			code: "eosio",
-					json: true,
-					limit: 1,
-					scope: "eosio",
-					table: "global"
-			  	})
-		  	},
-		  	function(data){
-  				success(data)
-  			}, 
-			function(textStatus) {error(textStatus)}
-  		)
-  	}
-
-  	function get_producers(success, error) {
-  		util.ajax(
-	  		{
-			    type: "post",
-			    url: url.rpc.get_table_rows,
-			    data: JSON.stringify({
-		            		"scope": "eosio",
-		            		"code":  "eosio",
-		                    "table": "producers",
-		                    "json":  "true",
-		                    "limit": 100,
-		                    "key_type": "float64",
-		                    "index_position": 2,
-		            }),
-			}, 
-			function(data){success(data)}, 
-			function(textStatus) {error(textStatus)}
-		)
-  	}
-
-  	function get_bp_info(i, bpname, success, error) {
-  		util.ajax(
-	  		{
-			    url: url.api.bp_info,
-			    data: {bpname: bpname},
-			}, 
-			function(data){success(i, bpname, data)}, 
-			function(textStatus) {error(textStatus)}
-		)
-  	}
-
-  	function get_producerjson(){
-	  	util.ajax({url: url.rpc.get_table_rows, data: 
-	  		JSON.stringify({
-	  			"json": "true",
-		  		"code": "producerjson",
-		  		"scope": "producerjson",
-		  		"table": "producerjson",
-		  		"limit": 1000
-		  	}), type: "POST"}
-	  	, function(data) {
-	  		for (var i = 0; i < data.rows.length; i++) {
-	  			var json = JSON.parse(data.rows[i].json);
-				var bpname = data.rows[i].owner;
-				  
-				if ("undefined" === typeof $scope.bp_honor[bpname]) {
-					$scope.bp_honor[bpname] = [];
-				}
-				$scope.bp_honor[bpname].push("已设置 JSON");
-
-				if ("undefined" !== typeof bpname2i[bpname]) {
-					items[bpname2i[bpname]] = Object.assign(items[bpname2i[bpname]], {json: json});
-					for (var j = 0; j < json.nodes.length; j++) {
-						util.check_http_or_https(bpname, json.nodes[j], "http", function (bpname, info) {
-							if (info.status == "ok") {
-								$scope.bp_honor[bpname].push("HTTP 接入点可用");
-								is_set = true;
-							}
-							items[bpname2i[bpname]]["http"] = Object.assign(items[bpname2i[bpname]]["http"], info)
-						});
-						util.check_http_or_https(bpname, json.nodes[j], "https", function (bpname, info) {
-							if (info.status == "ok") {
-								$scope.bp_honor[bpname].push("HTTPS 接入点可用");
-								is_set = true;
-							}
-							items[bpname2i[bpname]]["https"] = Object.assign(items[bpname2i[bpname]]["https"], info)
-						});
-						util.check_p2p(bpname, json.nodes[j], function (bpname, info) {
-							if (info.status == "ok") {
-								$scope.bp_honor[bpname].push("P2P 接入点可用");
-								is_set = true;
-							}
-							items[bpname2i[bpname]]["p2p"] = Object.assign(items[bpname2i[bpname]]["p2p"], info)
-						});
+						bpname2i[bp["owner"]] = i;
 					}
-				}
-	  		}
-			is_set = true;
-	  	}, function(){});
+					is_set = true;
+					get_producerjson();
+				}, function () { })
+			})
+		}
 
-  	}
+		function get_info() {
+			util.ajax({ url: url.rpc.get_info }, function (data) {
+				info = data;
+				$scope.info = info;
+				setTimeout(function () {
+					$(".progress-bar").width("100%");
+				}, 1);
+				$scope.$apply();
+				clearTimeout(st1);
+				st1 = setTimeout(function () {
+					get_info()
+				}, 1000)
+			}, function () { })
+		}
 
 
-  });
+		function set() {
+			$scope.items = items;
+			$scope.$apply();
+			// $(".tooltip").remove();
+			$('[data-toggle="tooltip"]').tooltip();
+		}
+
+		var si1 = setInterval(function () {
+			if (is_set) {
+				is_set = false;
+				set()
+			}
+		}, 200);
+
+		$scope.$on("$destroy", function () {
+			clearInterval(si1);
+			clearTimeout(st1);
+		})
+
+		function get_global(success, error) {
+			util.ajax(
+				{
+					url: url.rpc.get_table_rows,
+					type: "POST",
+					data: JSON.stringify({
+						code: "eosio",
+						json: true,
+						limit: 1,
+						scope: "eosio",
+						table: "global"
+					})
+				},
+				function (data) {
+					success(data)
+				},
+				function (textStatus) { error(textStatus) }
+			)
+		}
+
+		function get_producers(success, error) {
+			util.ajax(
+				{
+					type: "post",
+					url: url.rpc.get_table_rows,
+					data: JSON.stringify({
+						"scope": "eosio",
+						"code": "eosio",
+						"table": "producers",
+						"json": "true",
+						"limit": 100,
+						"key_type": "float64",
+						"index_position": 2,
+					}),
+				},
+				function (data) { success(data) },
+				function (textStatus) { error(textStatus) }
+			)
+		}
+
+		function get_bp_info(i, bpname, success, error) {
+			util.ajax(
+				{
+					url: url.api.bp_info,
+					data: { bpname: bpname },
+				},
+				function (data) { success(i, bpname, data) },
+				function (textStatus) { error(textStatus) }
+			)
+		}
+
+		function get_producerjson() {
+			util.ajax({
+				url: url.rpc.get_table_rows, data:
+					JSON.stringify({
+						"json": "true",
+						"code": "producerjson",
+						"scope": "producerjson",
+						"table": "producerjson",
+						"limit": 1000
+					}), type: "POST"
+			}
+				, function (data) {
+					for (var i = 0; i < data.rows.length; i++) {
+						var json = JSON.parse(data.rows[i].json);
+						var bpname = data.rows[i].owner;
+
+						if ("undefined" === typeof $scope.bp_honor[bpname]) {
+							$scope.bp_honor[bpname] = [];
+						}
+						$scope.bp_honor[bpname].push("已设置 JSON");
+
+						if ("undefined" !== typeof bpname2i[bpname]) {
+							items[bpname2i[bpname]] = Object.assign(items[bpname2i[bpname]], { json: json });
+							if (json.nodes) {
+								for (var j = 0; j < json.nodes.length; j++) {
+									util.check_http_or_https(bpname, json.nodes[j], "http", function (bpname, info) {
+										if (info.status == "ok") {
+											$scope.bp_honor[bpname].push("HTTP 接入点可用");
+											is_set = true;
+										}
+										items[bpname2i[bpname]]["http"] = Object.assign(items[bpname2i[bpname]]["http"], info)
+									});
+									util.check_http_or_https(bpname, json.nodes[j], "https", function (bpname, info) {
+										if (info.status == "ok") {
+											$scope.bp_honor[bpname].push("HTTPS 接入点可用");
+											is_set = true;
+										}
+										items[bpname2i[bpname]]["https"] = Object.assign(items[bpname2i[bpname]]["https"], info)
+									});
+									util.check_p2p(bpname, json.nodes[j], function (bpname, info) {
+										if (info.status == "ok") {
+											$scope.bp_honor[bpname].push("P2P 接入点可用");
+											is_set = true;
+										}
+										items[bpname2i[bpname]]["p2p"] = Object.assign(items[bpname2i[bpname]]["p2p"], info)
+									});
+								}
+							}
+						}
+					}
+					is_set = true;
+				}, function () { });
+
+		}
+
+	});
--- app/scripts/util.js
+++ app/scripts/util.js
@@ -23,12 +23,12 @@ var util = {
     ajax: function (options, success, error) {
         options = Object.assign({
             timeout: 5000,
-            tryCount : 0,
-            retryLimit : 3,
-            success: function (data, textStatus){
+            tryCount: 0,
+            retryLimit: 3,
+            success: function (data, textStatus) {
                 success(data);
             },
-            error: function (XMLHttpRequest, textStatus, errorThrown){
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
                 if (textStatus == 'timeout') {
                     this.tryCount++;
                     if (this.tryCount <= this.retryLimit) {
@@ -53,38 +53,38 @@ var util = {
 
 
     // 对象排序 顺序
-    compare_sort: function (pro) { 
-        return function (obj1, obj2) { 
-            var val1 = obj1[pro]; 
-            var val2 = obj2[pro]; 
-            if (val1 < val2 ) { //正序
-                return -1; 
-            } else if (val1 > val2 ) { 
-                return 1; 
-            } else { 
-                return 0; 
-            } 
-        } 
-    }, 
+    compare_sort: function (pro) {
+        return function (obj1, obj2) {
+            var val1 = obj1[pro];
+            var val2 = obj2[pro];
+            if (val1 < val2) { //正序
+                return -1;
+            } else if (val1 > val2) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    },
 
     // 对象排序 倒序
-    compare_reverse: function (pro) { 
-        return function (obj1, obj2) { 
-            var val1 = obj1[pro]; 
-            var val2 = obj2[pro]; 
-            if (val1 < val2 ) { //正序
-                return 1; 
-            } else if (val1 > val2 ) { 
-                return -1; 
-            } else { 
-                return 0; 
-            } 
-        } 
+    compare_reverse: function (pro) {
+        return function (obj1, obj2) {
+            var val1 = obj1[pro];
+            var val2 = obj2[pro];
+            if (val1 < val2) { //正序
+                return 1;
+            } else if (val1 > val2) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
     },
 
     // 复制对象
-    copy: function ( obj ){
-        return JSON.parse( JSON.stringify( obj ) );
+    copy: function (obj) {
+        return JSON.parse(JSON.stringify(obj));
     },
 
     // 投票统计
@@ -117,7 +117,7 @@ var util = {
         }
         if (vpay2 < 1000) {
             vpay2 = 0;
-        } 
+        }
         total = (rank <= 21) ? bpay2 + vpay2 : vpay2;
         unreceived = (next_claim_time > Date.now()) ? 0 : bpay + vpay;
         return {
@@ -125,17 +125,17 @@ var util = {
             unreceived: unreceived.toFixed(0)
         }
     },
-    
+
     // 时间格式化函数
-    timetrans: function (date){
+    timetrans: function (date) {
         var date = new Date(date);//如果date为13位不需要乘1000
         var Y = date.getFullYear() + '-';
-        var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+        var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
         var D = (date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate()) + ' ';
         var h = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':';
-        var m = (date.getMinutes() <10 ? '0' + date.getMinutes() : date.getMinutes()) + ':';
-        var s = (date.getSeconds() <10 ? '0' + date.getSeconds() : date.getSeconds());
-        return Y+M+D+h+m+s;
+        var m = (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()) + ':';
+        var s = (date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds());
+        return Y + M + D + h + m + s;
     },
 
     // 数组去重
@@ -151,115 +151,115 @@ var util = {
     },
 
 
-	check_http_or_https: function (bpname, bpinfo, type, callback) {
-		var endpoint = "";
-		if (type == "http") {
-			endpoint = bpinfo.api_endpoint || bpinfo.rpc_endpoint;
-		}
-		if (type == "https") {
-			endpoint = bpinfo.ssl_endpoint;
-		}
-		if (!endpoint) {
-			return;
-		}
-		if (type === "http") {
-			if (endpoint.indexOf("http://") !== 0) {
-				return callback(bpname, {status: "ng",msg: "not http",endpoint: endpoint});
-			}
-		}
-		if (type === "https") {
-			if (endpoint.indexOf("https://") !== 0) {
-				return callback(bpname, {status: "ng",msg: "not https",endpoint: endpoint});
-			}
-		}
-		callback(bpname, {status: "ing",msg:"connecting",endpoint: endpoint});
-		var url_get_info = endpoint + '/v1/chain/get_info';
-		var url_get_transaction = endpoint + '/v1/history/get_key_accounts';
-		// get_info
-		util.ajax({url: url_get_info}, function(info) {
-			if (info && info.head_block_num) {
-				// history
-				util.ajax({url: url_get_transaction, type: "POST", headers: {'content-type': 'application/json'}, data: '{"public_key":"FO6MzV92DgYjwDa7K3rtc28dPhGt2Gy8oUoHjESUq4gBx63v8num"}'},function(info) {
-					if (info) {
-						return callback(bpname, {history: true});
-					}
-				}, function(){})
-			  util.ajax({url: url_get_info, headers: {'content-type': 'application/json'}}, function() {}, function() {
-                callback(bpname, {cors: false});
-			  });
-				return callback(bpname, {status: "ok", msg: "",number: info.head_block_num,version: info.server_version_string});
-			} else {
-				return callback(bpname, {status: "ng",msg: "offline"});
-			}
-		}, function(textStatus) {
-			if (textStatus == "timeout") {
-				return callback(bpname, {status: "ng",msg:"timeout"});
-			} else {
-				return callback(bpname, {status: "ng",msg: "error"});
-			}
-		});
-	},
+    check_http_or_https: function (bpname, bpinfo, type, callback) {
+        var endpoint = "";
+        if (type == "http") {
+            endpoint = bpinfo.api_endpoint || bpinfo.rpc_endpoint;
+        }
+        if (type == "https") {
+            endpoint = bpinfo.ssl_endpoint;
+        }
+        if (!endpoint) {
+            return;
+        }
+        if (type === "http") {
+            if (endpoint.indexOf("http://") !== 0) {
+                return callback(bpname, { status: "ng", msg: "not http", endpoint: endpoint });
+            }
+        }
+        if (type === "https") {
+            if (endpoint.indexOf("https://") !== 0) {
+                return callback(bpname, { status: "ng", msg: "not https", endpoint: endpoint });
+            }
+        }
+        callback(bpname, { status: "ing", msg: "connecting", endpoint: endpoint });
+        var url_get_info = endpoint + '/v1/chain/get_info';
+        var url_get_transaction = endpoint + '/v1/history/get_key_accounts';
+        // get_info
+        util.ajax({ url: url_get_info }, function (info) {
+            if (info && info.head_block_num) {
+                // history
+                util.ajax({ url: url_get_transaction, type: "POST", headers: { 'content-type': 'application/json' }, data: '{"public_key":"FO6MzV92DgYjwDa7K3rtc28dPhGt2Gy8oUoHjESUq4gBx63v8num"}' }, function (info) {
+                    if (info) {
+                        return callback(bpname, { history: true });
+                    }
+                }, function () { })
+                util.ajax({ url: url_get_info, headers: { 'content-type': 'application/json' } }, function () { }, function () {
+                    callback(bpname, { cors: false });
+                });
+                return callback(bpname, { status: "ok", msg: "", number: info.head_block_num, version: info.server_version_string });
+            } else {
+                return callback(bpname, { status: "ng", msg: "offline" });
+            }
+        }, function (textStatus) {
+            if (textStatus == "timeout") {
+                return callback(bpname, { status: "ng", msg: "timeout" });
+            } else {
+                return callback(bpname, { status: "ng", msg: "error" });
+            }
+        });
+    },
 
-	check_p2p: function (bpname, bpinfo, callback) {
-		var endpoint = bpinfo.p2p_endpoint;
-		if (!endpoint) {
-			return;
-		}
-		endpoint = endpoint.replace("http://", "");
-		var addr = endpoint.split(":");
-		var host = addr[0];
-		var port = addr[1] || "";
+    check_p2p: function (bpname, bpinfo, callback) {
+        var endpoint = bpinfo.p2p_endpoint;
+        if (!endpoint) {
+            return;
+        }
+        endpoint = endpoint.replace("http://", "");
+        var addr = endpoint.split(":");
+        var host = addr[0];
+        var port = addr[1] || "";
 
-		callback(bpname, {status: "ing",msg:"connecting",endpoint: endpoint});
-		util.ajax({url: url.api.check_p2p, data: {host: host, port:port}}, function(data) {
-			var status = data && data.rows && data.rows.length;
-			if (!status) {
-				return callback(bpname, {status: "un",msg: "unknown",detecting: false});
-			}
+        callback(bpname, { status: "ing", msg: "connecting", endpoint: endpoint });
+        util.ajax({ url: url.api.check_p2p, data: { host: host, port: port } }, function (data) {
+            var status = data && data.rows && data.rows.length;
+            if (!status) {
+                return callback(bpname, { status: "un", msg: "unknown", detecting: false });
+            }
 
-			var rows = data.rows;
-			var msg = "";
-			var ok_num = 0;
-			var length = 1; // rows.length
+            var rows = data.rows;
+            var msg = "";
+            var ok_num = 0;
+            var length = 1; // rows.length
 
-			for (var i = 0; i < length; i++) {
-				if (
-					(rows[i].indexOf("connect ") >= 0 || 
-					rows[i].indexOf("connection ") >= 0 || 
-					rows[i].indexOf("self connect") >= 0 || 
-					rows[1].indexOf("self connect") >= 0
-					) 
-					&& rows[i].indexOf("failed ") === -1
-					&& port
-					) {
-					ok_num++;
-				}
-				var _msg = rows[i].split(port);
-				_msg = _msg[1] || _msg[0];
-				_msg = _msg.substr(1).replace(/\s+/g,"");
-				if (_msg) {
-					msg = rows[i].split(port);
-					msg = msg[1] || msg[0];
-					msg = msg.substr(1);
-				}
-				if (!port) {
-					msg = "Invalid peer address"
-				}
-			}
+            for (var i = 0; i < length; i++) {
+                if (
+                    (rows[i].indexOf("connect ") >= 0 ||
+                        rows[i].indexOf("connection ") >= 0 ||
+                        rows[i].indexOf("self connect") >= 0 ||
+                        rows[1].indexOf("self connect") >= 0
+                    )
+                    && rows[i].indexOf("failed ") === -1
+                    && port
+                ) {
+                    ok_num++;
+                }
+                var _msg = rows[i].split(port);
+                _msg = _msg[1] || _msg[0];
+                _msg = _msg.substr(1).replace(/\s+/g, "");
+                if (_msg) {
+                    msg = rows[i].split(port);
+                    msg = msg[1] || msg[0];
+                    msg = msg.substr(1);
+                }
+                if (!port) {
+                    msg = "Invalid peer address"
+                }
+            }
 
-			if (ok_num == length) {
-				return callback(bpname, {status: "ok",msg:"connect"});
-			} else {
-				return callback(bpname, {status: "ng",msg:msg});
-			}
-		}, function(textStatus){
-			if (textStatus == "timeout") {
-				callback(bpname, {status: "un",msg:"timeout"});
-			} else {
-				callback(bpname, {status: "un",msg: "unknown"});
-			}
-		})
-	}
+            if (ok_num == length) {
+                return callback(bpname, { status: "ok", msg: "connect" });
+            } else {
+                return callback(bpname, { status: "ng", msg: msg });
+            }
+        }, function (textStatus) {
+            if (textStatus == "timeout") {
+                callback(bpname, { status: "un", msg: "timeout" });
+            } else {
+                callback(bpname, { status: "un", msg: "unknown" });
+            }
+        })
+    }
 
 }
 
