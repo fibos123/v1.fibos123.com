be2cf9ad0e07630d0f84ba33e526d13c713e6c2f
mingfun wong <mingfun.wong.chn@gmail.com>
2019-03-24 21:53:10 +0800

    Updates

--- bp_honor.js
+++ bp_honor.js
@@ -1,10 +1,10 @@
 var bp_honor = {
 	"fibosrockskr": ["Fibos Rocks 浏览器", "DAPP 开发者"],
 	"fibos123comm": ["FIBOS 导航"],
-	"ansenironman": ["IronMan 作者"],
-	"ilovefibosbp": ["FOToolkit 作者"],
+	"ansenironman": ["IronMan"],
+	"ilovefibosbp": ["FOToolkit"],
 	"fibosisfunoo": ["DAPP 开发者"],
-
+	"superbpusphl": ["FO 财富排行榜"],
 	"liuqiangdong": ["FIBOS 数据备份"],
 	"zhangsenyuan": ["DAPP 开发者"],
 	"fibscandotio": ["FIBSCAN 浏览器"],
