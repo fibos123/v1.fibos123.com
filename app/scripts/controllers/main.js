a089b12db9f6ce5392a1299681f96fb47db92e10
mingfun wong <mingfun.wong.chn@gmail.com>
2019-03-24 22:23:46 +0800

    remove exchange info

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -36,32 +36,4 @@ angular.module('appApp')
     	}
     }, function (){})
 
-    var eos2fo, fo2eos;
-    var i = 0;
-
-    getExchangeInfo();
-
-    var si1 = setInterval(function () {
-      if (eos2fo) {
-        if (i % 2 === 0) {
-          document.title = eos2fo.toFixed(4) + ' FO / EOS | FIBOS 导航';
-        } else {
-          document.title = fo2eos.toFixed(6) + ' EOS / FO | FIBOS 导航';
-        }
-        i++;
-      }
-    }, 3 * 1000);
-
-    $scope.$on("$destroy", function () {
-      clearInterval(si1);
-    })
-
-    function getExchangeInfo() {
-      util.ajax({ url: "https://explorer.fibos.rocks/api/resource", dataType: "json" }, function (data) {
-        eos2fo = data.eos_fo_price;
-        fo2eos = 1 / data.eos_fo_price;
-        setTimeout(getExchangeInfo, 5000);
-      }, function () { })
-    }
-
   });
