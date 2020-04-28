e09ad97e13af33c9bd58084b6e9d34aac4245dd4
Mingfun Wong <mingfun.wong.chn@gmail.com>
2020-04-29 00:59:26 +0900

    Update config.example.js

--- app/fibos123comc/config.example.js
+++ app/fibos123comc/config.example.js
@@ -1,8 +1,7 @@
 const config = {
     client: {
-        chainId: 'cf057bbfb72640471fd910bcb67639c22df9f92470936cddc1ade0e2f2e7dc4f',
-        httpEndpoint: 'http://127.0.0.1:8888',
-        privateKey: '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'
+        chainId: '6aa7bd33b6b45192465afa3553dedb531acaaff8928cf64b70bd4c5e49b7ec6a',
+        httpEndpoint: 'http://api.fibos.rocks',
     },
     contract: {
         name: 'json',
@@ -12,17 +11,8 @@ const config = {
         name: 'fibos123comc',
         sender: 'fibos123comc'
     },
-    account: {
-        publicKey: 'FO6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV',
-        privateKey: '5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3'
-    },
     accountContract: {
-        publicKey: 'FO7REhLt8kCDbWkdrMeBjM1RBJvuPpCm1bdpWZWQVJtGPbM2gkZw',
-        privateKey: '5JtD3kWNMj8MFbjt8nJXbiTDDpapGEdJ4KH57ouJbTTo8CNemTM'
-    },
-    accountAttacker: {
-        publicKey: 'FO5brLzqwAEjudbSHEXT45ZQcTR4gecefJmb8KiASvBfmWQVD9VX',
-        privateKey: '5K4HY3He1wfgVfKfiUWXJiUYQkDPeJL5k7cD2cjppHfpY917oNs'
+        privateKey: 'xxxxxxxxxxxxxxxxxxxxxxxxx'
     }
 }
 
