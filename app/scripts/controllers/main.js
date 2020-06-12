7eafd83b15629e6f654eb1a88bebe002d5c8b395
mingfun wong <mingfun.wong.chn@gmail.com>
2020-06-12 22:08:04 +0900

    refine service

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,27 +13,27 @@ angular.module('appApp')
     document.title = 'FIBOS 导航';
     $(window).scrollTop(0)
 
-    // util.ajax({ url: 'fibos123comc/sites.json' }
-    //   , function (data) {
-    //     $scope.data = data
-    //     $scope.$apply();
-    //   }, function () { })
+    util.ajax({ url: 'fibos123comc/sites.json' }
+      , function (data) {
+        $scope.data = data
+        $scope.$apply();
+      }, function () { })
 
-    util.ajax({url: url.rpc.get_table_rows, data:
-    	JSON.stringify({
-    		"json": "true",
-    		"code": "fibos123comc",
-    		"scope": "fibos123comc",
-    		"table": "jsons",
-    		"limit": 1,
-    		"lower_bound": "sites",
-    	}), type: "POST"}
-    , function(data) {
-    	if (data && data.rows && data.rows[0] && data.rows[0].text) {
-    		var websites = data.rows[0].text;
-    		$scope.data = JSON.parse(websites);
-    		$scope.$apply();
-    	}
-    }, function (){})
+    // util.ajax({url: url.rpc.get_table_rows, data:
+    // 	JSON.stringify({
+    // 		"json": "true",
+    // 		"code": "fibos123comc",
+    // 		"scope": "fibos123comc",
+    // 		"table": "jsons",
+    // 		"limit": 1,
+    // 		"lower_bound": "sites",
+    // 	}), type: "POST"}
+    // , function(data) {
+    // 	if (data && data.rows && data.rows[0] && data.rows[0].text) {
+    // 		var websites = data.rows[0].text;
+    // 		$scope.data = JSON.parse(websites);
+    // 		$scope.$apply();
+    // 	}
+    // }, function (){})
 
   });
